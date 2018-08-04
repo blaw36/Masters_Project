d4626c25f76d712df94e291bd1981572f6ac79a5
Brendan Law <law.bkh@gmail.com>
2018-08-05 00:05:18 +1000

    added a basic_commands for reference, and a readme for the project

---
+++ basic_commands.R
@@ -0,0 +1,19 @@
+### Basic commands
+
+library(workflowr)
+
+# Find all the markdown files in the project which have been updated, and re-build them.
+wflow_build()
+# Find the status of the markdown files and whether they're ready to be published, or whether they are up-to-date
+wflow_status()
+# 'Unp = Unpublished' <- html files not committed to git repo yet
+wflow_publish("analysis/*Rmd") # to publish the unpublished stuff
+
+# We've only committed(published) to our local repository.
+# Create a remote (and call it the default - origin)
+# # Only need to run it once (at the start)
+# wflow_git_remote(remote = "origin"
+#                  ,user = "blaw36"
+#                  ,repo = "Masters_Project")
+# Now we'll push to our git repo online.
+wflow_git_push()
